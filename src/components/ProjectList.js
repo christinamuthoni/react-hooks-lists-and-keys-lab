0f019903343c02e58ad87b9e8ba05647a768fc7f
christine <christine.muriungi@student.moringaschool.com>
2023-11-09 09:12:02 +0300

    completed assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,9 +6,19 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+      {projects.map(projects => (
+        <ProjectItem
+          key={projects.id}
+          id={projects.id}
+          name={projects.name}
+          description={projects.description} />
+      ))}
     </div>
-  );
-}
+    </div>
+  )
+      }
+  
+
 
 export default ProjectList;
